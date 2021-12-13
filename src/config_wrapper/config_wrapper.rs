b313bc55f46bbe6a5aa7e650fe96c485ef039e44
tee8z <tee8z@protonmail.com>
2021-12-12 22:50:38 -0500

    T8:Removed no longer needed settings from config

--- src/config_wrapper/config_wrapper.rs
+++ src/config_wrapper/config_wrapper.rs
@@ -9,8 +9,6 @@ lazy_static::lazy_static! {
 pub struct Settings {
     pub sock_url_local: String,
     pub socks_port_local: u16, 
-    pub check_url: String,
-    pub macaroon: String,
     pub telegram_bot_id: String,
     pub telegram_base_url: String,
 }
@@ -21,8 +19,6 @@ impl Default for Settings {
         Settings {
             sock_url_local: "socks5h://127.0.0.1".to_string(),
             socks_port_local: 9056,
-            check_url: "".to_string(),
-            macaroon: "".to_string(),
             telegram_bot_id: "".to_string(),
             telegram_base_url: "".to_string()
         }
@@ -39,8 +35,6 @@ impl Settings{
         let mut settings = Settings::default();
         settings.sock_url_local = def_config.get_str("SOCKS_URL_LOCAL").unwrap();
         settings.socks_port_local = def_config.get("SOCKS_PORT_LOCAL").unwrap();
-        settings.check_url = def_config.get_str("CHECK_URL").unwrap();
-        settings.macaroon = def_config.get_str("MACAROON").unwrap();
         settings.telegram_bot_id = def_config.get_str("TELEGRAM_BOT_ID").unwrap();
         settings.telegram_base_url = def_config.get_str("TELEGRAM_BASE_URL").unwrap();
         return settings;
